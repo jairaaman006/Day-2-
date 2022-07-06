1a53767d58b4f75016dc6e1836d6a035a5ac040d
jairaaman006 <99857978+jairaaman006@users.noreply.github.com>
2022-07-06 23:26:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1. Write a blog on the difference between document vs window object?
+
+// document object
+
+// *The document object represents a web page that is loaded in the browser.
+// *By accessing the document object,we can access the element in html page.We can add dynamic content to our page.
+// *It is accessed with window.document or document. Syntax- document.property_name
+
+// Window object
+
+// *Window object is the topmost object in DOM hierarchy.
+// *It represents a browser window or a framework that displays the contents of the webpage.
+// *Whenever a window appears on screen to display the contents of the document,window object is created. Syntax-window.document_name.
